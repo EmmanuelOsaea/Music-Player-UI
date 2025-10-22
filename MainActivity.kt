638204ff959bd7c7c1e338f25808ce92ab791872
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-22 09:50:26 +0100

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.musicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isPlaying = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener {
+            isPlaying = !isPlaying
+            binding.playButton.text = if (isPlaying) "Pause" else "Play"
+        }
+    }
+}
